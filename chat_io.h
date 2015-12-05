68c625757679e75acb3bd7558611f95c5d0ebecc
Ilan Smith <ilan.smith@sandisk.com>
2015-12-06 00:40:48 +0200

    Added colour support.

--- chat_io.h
+++ chat_io.h
@@ -55,6 +55,76 @@ typedef enum {
 #define MSG_VAL_COLLEGUE_ID(MSG) (*((long *)(MSG_VAL(MSG))))
 #define MSG_VAL_COLLEGUE_NM(MSG) ((char *)(MSG_VAL(MSG) + sizeof(long)))
 
+/* colours */
+#define COL_RESET "\033[00;00m"
+/* #define COL_BLACK "\033[00;30m" */
+#define COL_BRIGHT_BLACK "\033[01;30m"
+/* #define COL_RED "\033[00;31m" */
+#define COL_BRIGHT_RED "\033[01;31m"
+/* #define COL_GREEN "\033[00;32m" */
+#define COL_BRIGHT_GREEN "\033[01;32m"
+/* #define COL_ORANGE "\033[00;33m" */
+#define COL_YELLOW "\033[01;33m"
+/* #define COL_BLUE "\033[00;34m" */
+#define COL_BRIGHT_BLUE "\033[01;34m"
+/* #define COL_MAGENTA "\033[00;35m" */
+#define COL_BRIGHT_MAGENTA "\033[01;35m"
+/* #define COL_CYAN "\033[00;36m" */
+#define COL_BRIGHT_CYAN "\033[01;36m"
+/* #define COL_GREY "\033[00;37m" */
+/* #define COL_BRIGHT_GREY "\033[01;37m" */
+/* #define COL_WHITE "\033[00;38m" */
+#define COL_BRIGHT_WHITE "\033[01;38m"
+#define COL_BG_BLACK "\033[00;40m"
+#define COL_BG_BRIGHT_RED "\033[01;41m"
+
+/* colour definitions */
+#define COLOUR_DEBUG COL_BRIGHT_BLACK 
+#define COLOUR_APP_NAME COL_BRIGHT_CYAN 
+#define COLOUR_ERROR_PROMPT COL_BG_BRIGHT_RED
+#define COLOUR_ERROR COL_YELLOW
+#define COLOUR_CLIENT_PROMPT COL_BRIGHT_CYAN 
+#define COLOUR_CLIENT COL_BRIGHT_BLUE 
+/* #define COLOUR_DAEMON COL_BRIGHT_WHITE */
+#define COLOUR_FEEDBACK COL_BRIGHT_GREEN
+#define COLOUR_PEER_PROMPT COL_BRIGHT_RED 
+#define COLOUR_PEER COL_BRIGHT_MAGENTA 
+#define COLOUR_CLEAR COL_BRIGHT_WHITE
+
+#define COLOUR_RESET                                                          \
+    fprintf(stdout, "%s", COL_RESET);                                         \
+    fflush(stdout);                                                           \
+    fprintf(stderr, "%s", COL_RESET);                                         \
+    fflush(stderr);
+
+#define SET_COLOUR(STREAM, COLOUR)                                            \
+    fprintf(STREAM, "%s%s", COL_BG_BLACK, COLOUR);                            \
+    fflush(STREAM);
+
+#define PRINT_FEEDBACK(FMT, ...)                                              \
+    SET_COLOUR(stdout, COLOUR_FEEDBACK);                                      \
+    fprintf(stdout, FMT "\n", ##__VA_ARGS__);                                 \
+    SET_COLOUR(stdout, COLOUR_CLEAR);
+
+#define PRINT_ERROR(FMT, ...)                                                 \
+    SET_COLOUR(stderr, COLOUR_CLEAR);                                         \
+    SET_COLOUR(stderr, COL_BG_BRIGHT_RED);                                    \
+    fprintf(stderr, "error:");                                                \
+    SET_COLOUR(stderr, COLOUR_CLEAR);                                         \
+    SET_COLOUR(stderr, COL_YELLOW);                                           \
+    fprintf(stderr, " " FMT "\n", ##__VA_ARGS__);                             \
+    SET_COLOUR(stderr, COL_RESET);
+
+#define PRINT_INTRO(FMT, ...)                                                 \
+    SET_COLOUR(stdout, COLOUR_CLEAR);                                         \
+    fprintf(stdout, FMT "\n", ##__VA_ARGS__);                                 \
+    SET_COLOUR(stdout, COLOUR_CLEAR);
+
+#define PRINT_DEBUG(FMT, ...);                                                \
+    SET_COLOUR(stdout, COLOUR_DEBUG);                                         \
+    fprintf(stdout, FMT "\n", ##__VA_ARGS__);                                 \
+    SET_COLOUR(stdout, COL_RESET);
+
 typedef struct msg_t {
     tlv_t type;
     int length;
